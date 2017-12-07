18304eff65b6ea0043695c955a0369ceaf1aa400
Justin <justinhbaker3@gmail.com>
2017-12-07 13:53:22 -0600

    Initial commit

--- Markov Andersen.py
+++ Markov Andersen.py
@@ -1,7 +1,23 @@
-import Simulation as sim
 import numpy as np
 
 p_choose_particle = 0.01
 
-def MA_step(R, V, T, M):
-    proposed_velocity = np.random.
+def MA_step(V, T, M):
+    sigma = (T/M)**0.5
+
+    for i in range(0, len(V)):
+        if p_choose_particle > np.random.ranf():    #choose particles with probability p_choose_particle
+            proposed_velocity = sigma*np.random.randn(3)    #choose propoesed velocity on the gaussian distribution
+
+            acc_prob = np.absolute(proposed_velocity-V[i])/V[i]         #accecptance probability inverse of percent change
+            acc_check = np.random.ranf(3)
+
+            if acc_prob[0] > acc_check[0]:
+                V[i][0] = proposed_velocity[0]          #update V[i]x
+            if acc_prob[1] > acc_check[1]:
+                V[i][1] = proposed_velocity[1]          #update V[i]y
+            if acc_prob[2] > acc_check[2]:
+                V[i][2] = proposed_velocity[2]          #update V[i]z
+
+
+
